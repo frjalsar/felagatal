7a915f797b441bd5d7649779acebf3e1ecb9cf86
Bergur Hallgrimsson <bergur@bergur.is>
2019-01-09 11:11:44 +0000

    Kdud script og skilgreina node version

--- deploy.sh
+++ deploy.sh
@@ -114,10 +114,6 @@ if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
   echo "Running $NPM_CMD install"
   eval $NPM_CMD install
-  echo "Running $NPM_CMD run build"
-  eval $NPM_CMD run build
-  echo "Running $NPM_CMD prune --production"
-  eval $NPM_CMD prune --production
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
 fi
