e3328b1e0707dc9a7181307f3cccf64e65bfaf9d
Bergur Hallgrimsson <bergur@bergur.is>
2019-08-11 01:01:10 +0000

    Heroku hack fyrir secure cookies

--- server.js
+++ server.js
@@ -3,23 +3,14 @@ const compression = require('compression')
 const bodyParser = require('body-parser')
 
 const app = express()
-
+app.set('trust proxy', 1) // proxy hakk fyrir secure cookies
 app.use(compression())
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }))
-
-app.get('/test', (req, res) => {
-  res.json({ test: true })
-})
 
 app.use(express.static('dist', {
   maxAge: '1y'
 }))
-
-app.get('/test', (req, res) => {
-  res.json({ test: true })
-})
 
 app.set('port', process.env.PORT || 3010)
 app.listen(app.get('port'), () => {
