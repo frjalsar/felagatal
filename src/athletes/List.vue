30fe4d9af77c88ab1089f7b8fecbf4898fdfae49
Bergur Hallgrímsson <bergur@bergur.is>
2019-09-16 02:46:03 +0000

    Search is now in query for history

--- src/athletes/List.vue
+++ src/athletes/List.vue
@@ -1,87 +1,13 @@
 <template>
   <div>
     <h1>Iðkendur</h1>
-    <div class="row mb-4">
-      <div class="col-md-12">
-        <a class="btn btn-sm" v-for="letter in alphabet" :key="letter" @click="letterSearch(letter)">
-          {{ letter }}
-        </a>
-      </div>
-    </div>
-    <div class="row mb-4">
-      <div class="col-md-3">
-        <input
-          type="text"
-          v-model="query.search"
-          class="form-control text-center"
-          placeholder="Leita"
-          @keyup="search"
-        >
-      </div>
-      <div class="col-md-3">
-        <select
-          v-model="query.regionId"
-          class="form-control"
-          @change="search"
-        > 
-          <option
-            selected="selected"
-            :value="undefined"
-          >
-            Íþróttahérað
-          </option>
-          <option
-            v-for="region in regions"
-            :key="region.id"
-            :value="region.id"
-          >
-          {{ region.fullName }} 
-          </option>
-        </select>
-      </div>
-      <div class="col-md-3">
-        <select
-          v-model="query.clubId"
-          class="form-control"
-          @change="search"
-        > 
-          <option
-            selected="selected"
-            :value="undefined"
-          >
-            Félag
-          </option>
-          <option
-            v-for="club in filteredClub"
-            :key="club.id"
-            :value="club.id"
-          >
-          {{ club.fullName }} 
-          </option>
-        </select>
-      </div>
-      <div class="col-md-3">
-        <select
-          v-model="query.legacyClub"
-          class="form-control"
-          @change="search"
-        > 
-          <option
-            selected="selected"
-            :value="undefined"
-          >
-            Gömul félagaskráning
-          </option>
-          <option
-            v-for="club in legacy"
-            :key="club"
-            :value="club"
-          >
-          {{ club }} 
-          </option>
-        </select>
-      </div>
-    </div>
+    <SearchPanel
+      :regions="regions"
+      :clubs="clubs"
+      :legacy="legacy"
+      :default="settings"
+      @change="setQueryParams"
+      />
     <div class="row">
       <table class="table">
         <tr>
@@ -104,7 +30,7 @@
           <td>{{ athlete.id }}</td>
           <td>{{ athlete.fullName }}</td>
           <td>{{ athlete.birthyear }}</td>
-          <td>{{ athlete.club && athlete.club.fullName }}</td>
+          <td>{{ getCurrentClub(athlete) }}</td>
           <td>{{ athlete.country }}</td>
         </tr>
       </table>
@@ -114,12 +40,14 @@
 
 <script>
 import { FadeLoader } from '@saeris/vue-spinners'
+import SearchPanel from './SearchPanel'
 import agent from 'superagent'
 
 export default {
   name: 'AthletesList',
   components: {
-    FadeLoader
+    FadeLoader,
+    SearchPanel
   },
   data() {
     return {
@@ -128,61 +56,46 @@ export default {
       clubs: [],
       regions: [],
       legacy: [],
-      loadCount: 0,
-      query: {
-        search: undefined,
-        regionId: undefined,
-        clubId: undefined,
-        legacyClub: undefined
-      },      
-      alphabet: ['A', 'Á', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'Í', 'J', 'K', 'L', 'M', 'N', 'O', 'Ó', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'V', 'W', 'X', 'Y', 'Ý', 'Z', 'Þ', 'Æ', 'Ö'],
-    }
-  },
-  computed: {
-    filteredClub() {
-      if (this.query.regionId > 0) {
-        return this.clubs.filter(club => club.regionId === this.query.regionId)
-      } else {
-        return this.clubs
-      }
-    },
-    emptySearch() {
-      return !(this.query.search || this.query.regionId || this.query.clubId || this.query.legacyClub || this.query.startsWith)
     }
   },
   methods: {
+    getCurrentClub(athlete) {
+      if (athlete.membership && athlete.membership.length > 0) {
+        const foundClub = athlete.membership.find(m => m.current)
+        return foundClub ? foundClub.fullName : ''
+      }
+      return ''
+    },
+    setQueryParams (query) {
+      console.log('set query', query)
+      this.$router.replace({ query })
+      this.search()
+    },
     onClick (item) {
       this.$router.push('/idkendur/' + item.id)
     },
-    letterSearch(letter) {
-      this.query = {
-        startsWith: letter
-      }
-      return this.search().then(() => {
-        this.query.startsWith = undefined
-      })
-    },
     search () {
+      console.log('searching')
       this.busy = true
       this.athletes = []
-      if (this.emptySearch) {        
-        return Promise.resolve([]).then(res => {
-          this.athletes = res
-          this.busy = false
-        })       
-      }
       return agent
         .get(process.env.FRI_API_URL + '/athletes')
         .withCredentials()
-        .query(this.query)
+        .query(this.$route.query)
         .then(res => {
           this.athletes = res.body
-
           this.busy = false
         })      
     }
   },
+  computed: {
+    settings() {
+      return this.$route.query
+    }
+  },
   mounted() {
+    this.search()
+
     agent
       .get(process.env.FRI_API_URL + '/clubs')
       .withCredentials()
---
+++ src/athletes/SearchPanel.vue
@@ -0,0 +1,180 @@
+<template>
+  <div>
+    <div class="row mb-4">
+      <div class="col-md-12">
+        <a          
+          class="btn btn-sm"
+          v-for="letter in alphabet"
+          :key="letter"
+          :class="{'btn-outline-secondary': startsWith === letter}"
+          @click="toggleLetter(letter)"
+        >
+          {{ letter }}
+        </a>
+      </div>
+    </div>
+    <div class="row mb-4">
+      <div class="col-md-3">
+        <input
+          :value="search"
+          type="text"
+          class="form-control text-center"
+          placeholder="Leita"
+          @keyup="searchInput"
+        >
+      </div>
+      <div class="col-md-3">
+        <select
+          :value="regionId"
+          class="form-control"
+          @change="changeRegion"
+        > 
+          <option
+            selected="selected"
+            :value="undefined"
+          >
+            Íþróttahérað
+          </option>
+          <option
+            v-for="region in regions"
+            :key="region.id"
+            :value="region.id"
+          >
+          {{ region.fullName }} 
+          </option>
+        </select>
+      </div>
+      <div class="col-md-3">
+        <select
+          :value="clubId"
+          class="form-control"
+          @change="changeClub"
+        > 
+          <option
+            selected="selected"
+            :value="undefined"
+          >
+            Félag
+          </option>
+          <option
+            v-for="club in filteredClubs"
+            :key="club.id"
+            :value="club.id"
+          >
+          {{ club.fullName }} 
+          </option>
+        </select>
+      </div>
+      <div class="col-md-2">
+        <select
+          :value="legacyClub"
+          class="form-control"
+          @change="changeLegacy"
+        > 
+          <option
+            selected="selected"
+            :value="undefined"
+          >
+            Gömul skráning
+          </option>
+          <option
+            v-for="club in legacy"
+            :key="club"
+            :value="club"
+          >
+          {{ club }} 
+          </option>
+        </select>
+      </div>
+      <div class="col-md-1">
+        <button
+          type="button"
+          class="btn btn-secondary"
+          @click="clear"
+        >
+        Hreinsa
+        </button>
+      </div>
+    </div>
+</template>
+
+<script>
+import debounce from 'lodash.debounce'
+
+export default {
+  name: 'AthleteSearchPanel',
+  props: {
+    regions: Array,
+    clubs: Array,
+    legacy: Array,
+    default: Object
+  },
+  data() {
+    console.log(this.settings)
+    return {
+      startsWith: this.default.startsWith,
+      search: this.default.search,
+      regionId: this.default.regionId,
+      clubId:  this.default.clubId,
+      legacyClub: this.default.legacyClub,
+      alphabet: ['A', 'Á', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'Í', 'J', 'K', 'L', 'M', 'N', 'O', 'Ó', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'V', 'W', 'X', 'Y', 'Ý', 'Z', 'Þ', 'Æ', 'Ö'],
+    }
+  },
+  computed: {
+    selected() {
+      return {
+        startsWith: this.startsWith,
+        search: this.search,
+        regionId: this.regionId,
+        clubId: this.clubId,
+        legacyClub: this.legacyClub
+      }
+    },
+    filteredClubs() {
+      if (this.regionId > 0) {
+        return this.clubs.filter(club => club.regionId === Number(this.regionId))
+      } else {
+        return this.clubs
+      }
+    },
+  },
+  methods: {
+    clear() {
+      this.startsWith = undefined
+      this.search = undefined
+      this.regionId = undefined
+      this.clubId = undefined
+      this.legacyClub = undefined
+      this.$emit('change', this.selected)
+    },
+    toggleLetter(letter) {
+      if (this.startsWith === letter) {
+        this.startsWith = undefined
+      } else {
+        this.startsWith = letter
+      }
+      this.$emit('change', this.selected)
+    },
+    searchInput: debounce(function(e) {
+      this.startsWith = undefined
+      this.search = e.target && e.target.value
+      if (this.search.length >= 3) {
+        this.$emit('change', this.selected)
+      }
+    },300),
+    changeRegion(e) {
+      this.regionId = e.target.value
+      this.clubId = undefined
+      this.$emit('change', this.selected)
+    },
+    changeClub(e) {
+      this.clubId = e.target.value
+      this.$emit('change', this.selected)
+    },
+    changeLegacy(e) {
+      this.legacyClub = e.target.value
+      this.$emit('change', this.selected)
+    }
+  }  
+}
+</script>
