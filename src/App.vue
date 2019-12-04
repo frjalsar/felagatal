0c472750c60550bb27d4d8ad6d15dfde101136f0
Bergur Hallgrímsson <bergur@bergur.is>
2019-12-04 19:50:12 +0000

    Increasing shadow on main box

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow">
       <a href="/#/">
         <img
           alt="Frjálsíþróttasamband Íslands"
